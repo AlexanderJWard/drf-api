e59673c7f33d448cb5f5a84ce779e74781ff981a
AlexanderJWard <alexjamesward@hotmail.com>
2023-03-28 11:27:27 +0000

    Update settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -64,7 +64,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEV" in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'ajw-drf-api.herokuapp.com']
 
 
 # Application definition
